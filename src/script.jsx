f37ca5892cbea45397b82c40b8a4707b5a9711c2
barrosdvlpr <barrosdvlpr@gmail.com>
2023-02-03 09:15:36 -0300

    Implementing Store from Scratch

--- src/script.jsx
+++ src/script.jsx
@@ -9,15 +9,31 @@ const counter = (state = 0, action) => {
     }
   } 
   
-  const { createStore } = Redux;
-  // var createStore = Redux.createStore;
-  // import { createStore } from 'redux';
+  const createStore = (reducer) => {
+    let state;
+    let listeners = [];
+    
+    const getState = () => state;
+    
+    const dispatch = (action) => {
+      state = reducer(state, action);
+      listeners.forEach(listener => listener());
+    };
+    
+    const subscribe = (listener) => {
+      listeners.push(listener);
+      return () => {
+        listeners = listeners.filter(l => l !== listener);
+      };
+    };
+    
+    dispatch({});
+    
+    return { getState, dispatch, subscribe };
+  };
 
   const store = createStore(counter);
 
-  // store.dispatch({type: 'INCREMENT',})
-  // console.log(store.getState());
-  
   const render = () => {
     document.body.innerText = store.getState();
   };
