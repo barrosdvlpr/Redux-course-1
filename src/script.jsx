c5b0508ad7214a663d820531607fc14d48eca4ab
barrosdvlpr <barrosdvlpr@gmail.com>
2023-02-06 09:20:30 -0300

    Avoiding Array Mutations use concat, slice, spread

--- src/script.jsx
+++ src/script.jsx
@@ -1,50 +1,79 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
+const addCounter = (list) => {
+    return [...list, 0];
+  };
+  
+  const removeCounter = (list, index) => {
+    return [
+      ...list.slice(0, index),
+      ...list.slice(index + 1)
+    ];
+  };
+  
+  const incrementCounter = (list, index) => {
+    return [
+      ...list.slice(0, index),
+      list[index] + 1,
+      ...list.slice(index + 1)
+    ];
+  };
+  
+  const testAddCounter = () => {
+    const listBefore = [];
+    const listAfter = [0];
+    
+    deepFreeze(listBefore);
+    
+    expect(
+      addCounter(listBefore)
+    ).toEqual(listAfter);
+  };
+  
+  const testRemoveCounter = () => {
+    const listBefore = [0, 10, 20];
+    const listAfter = [0, 20];
+    
+    deepFreeze(listBefore);
+    
+    expect(
+      removeCounter(listBefore, 1)
+    ).toEqual(listAfter);
+  };
+  
+  const testIncrementCounter = () => {
+    const listBefore = [0, 10, 20];
+    const listAfter = [0, 11, 20];
+    
+    deepFreeze(listBefore);
+    
+    expect(
+      incrementCounter(listBefore, 1)
+    ).toEqual(listAfter);
+  };
+  
+  testAddCounter();
+  testRemoveCounter();
+  testIncrementCounter();
+  
+  console.log('All tests passed.') || displayInPreview('All tests passed.');
+  
+  
+  
+  
+  // display in plunker preview
+  function displayInPreview(string) {
+    var newDiv = document.createElement("div"); 
+    var newContent = document.createTextNode(string); 
+    newDiv.appendChild(newContent);
+    document.body.appendChild(newDiv)
   }
-} 
-
-export const Counter = ({
-  value,
-  onIncrement,
-  onDecrement
-}) => (
-  <div>
-    <h1>{value}</h1>
-    <button onClick={onIncrement}>+</button>
-    <button onClick={onDecrement}>-</button>
-  </div>
-);
-
-const store = createStore(counter);
-
-const render = () => {
-  ReactDOM.render(
-    <Counter
-      value={store.getState()}
-      onIncrement={() =>
-        store.dispatch({
-          type: 'INCREMENT'           
-        })            
-      }
-      onDecrement={() =>
-        store.dispatch({
-          type: 'DECREMENT'           
-        })            
-      }
-    />,
-    document.getElementById('root')
-  );
-};
-
-store.subscribe(render);
-render();
+  
+  // Function exported from deep-freeze lib
+  function deepFreeze (o) {
+    if (o===Object(o)) {
+      Object.isFrozen(o) || Object.freeze(o)
+      Object.getOwnPropertyNames(o).forEach(function (prop) {
+        prop==='constructor'||deepFreeze(o[prop])
+      })
+    }
+    return o
+  }
