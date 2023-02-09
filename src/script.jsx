cdd1d16ead64658fe6fbfbc8baa0aad0cd9587fa
barrosdvlpr <barrosdvlpr@gmail.com>
2023-02-09 08:33:34 -0300

    React Todo List Example (Toggling a Todo)

--- src/script.jsx
+++ src/script.jsx
@@ -79,7 +79,19 @@ class TodoApp extends Component {
         </button>
         <ul>
           {this.props.todos.map(todo =>
-            <li key={todo.id}>
+            <li key={todo.id}
+                onClick={() => {
+                  store.dispatch({
+                    type: "TOGGLE_TODO",
+                    id: todo.id
+                  });         
+                }}
+                style={{
+                  textDecoration:
+                    todo.completed ?
+                      "line-through" :
+                      "none"
+                }}>
               {todo.text}
             </li>
           )}
@@ -94,7 +106,7 @@ const render = () => {
     <TodoApp
       todos={store.getState().todos}
     />,
-    document.getElementById('root')
+    document.getElementById("root")
   );
 };
 
